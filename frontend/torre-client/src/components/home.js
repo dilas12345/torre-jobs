eb250e5f3d04b61f4157ac2ad74a874a132b9277
che <josephdilas@gmail.com>
2021-06-18 14:20:50 +0100

    home settup and working

---
+++ frontend/torre-client/src/components/home.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+    render(){
+        return(
+            <div>
+                <header>
+                    <h3>
+                        This is very first Home.
+                    </h3>
+                </header>
+            </div>
+        )
+    }
+}
+
+export default Home;
---
+++ frontend/torre-client/src/components/register.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+    render(){
+        return(
+            <div>
+                <header>
+                    <hr>
+                        This is very first Home.
+                    </hr>
+                </header>
+            </div>
+        )
+    }
+}
+
+export default Home;
