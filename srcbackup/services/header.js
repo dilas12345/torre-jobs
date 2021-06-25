1771d2c1653dbb95bd6988c9c601edce5ddc947d
che <josephdilas@gmail.com>
2021-06-25 08:46:30 +0100

    sync to remote repo

--- srcbackup/services/header.js
+++
@@ -1,9 +0,0 @@
-export default function header() {
-    const user = JSON.parse(localStorage.getItem('user'));
-
-    if (user && user.accessToken) {
-        return{ 'x-access-token': user.accessToken };
-    } else {
-        return {};
-    }
-}
